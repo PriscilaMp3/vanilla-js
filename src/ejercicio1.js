f7bd70231ee66da0f3bea9740a3fa1522f2962f7
Priscilla Medina <pmedinap@fwdcostarica.com>
2023-06-20 14:54:23 -0600

    respuestas

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,9 @@
 let contador = {
   valor: 0,
   siguiente: function () {
-    // Inserte el código aquí
+    this.valor += 1;
+    return this.valor;
   },
 };
 
-export { contador }
+export { contador };
