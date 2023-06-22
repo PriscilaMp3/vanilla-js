c32627fd931a85a23efc4e9995835aad5b24bb31
Priscilla Medina <pmedinap@fwdcostarica.com>
2023-06-22 15:29:39 -0600

    ejercicio-4

---
+++ src/calculadora.js
@@ -0,0 +1,20 @@
+function sumar(n1, n2) {
+  return n1 + n2;
+}
+function restar(n1, n2) {
+  return n1 - n2;
+}
+
+function multiplicar(n1, n2) {
+  return n1 * n2;
+}
+
+function dividir(n1, n2) {
+  return n1 / n2;
+}
+
+let calculadora={
+    sumar, restar, multiplicar, dividir
+}
+
+export {calculadora};
